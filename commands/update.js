dad2d21e292243b1aecce6dcf9e6ee107b974928
Lechienjade <103793801+Lechienjade@users.noreply.github.com>
2022-06-07 00:39:55 -0500

    first commit

---
+++ commands/update.js
@@ -0,0 +1,13 @@
+// const guildMemberAdd = require("../events/guild/guildMemberAdd");
+
+// module.exports = {
+//     name: 'update',
+//     description: "Adds new ranks",
+//     execute(client, message, args, Discord, profileData){
+//         ('guildMemberAdd', guildMember =>{const profile = profileData.points
+//         let role1 = guildMember.guild.roles.cache.find(role => role.name === 'Menig');
+//         if (profile == 2 && !(profile > 4)) guildMember.roles.add(role1);
+        
+//         })
+//     }
+// }
